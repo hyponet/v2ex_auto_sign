dc1aab01cc6a7c8984837909178f8f3149f1185a
hhHypo <i@ihypo.net>
2016-08-11 15:32:28 +0800

    u

--- start.py
+++ start.py
@@ -4,11 +4,6 @@ import time
 
 from sign import sign
 
-"""
-一个可以放在后台执行的死循环，
-每执行一次便会SLEEP一天，从而实现每天自动备份数据库，
-但是DaoCloud的胶囊主机如果长时间不访问会挂起，因此这个并不适用
-"""
 
 if __name__ == '__main__':
     SLEEPTIME = 60 * 60 * 24
